0f01eb9b01b910e09d39ed435d4e70e9fc8f3f4d
Dany Sluijk <me@dany.dev>
2024-05-25 22:51:30 +0200

    feat: add cache for geocode lookups

--- frontend/server/lib/mapbox/geocode.ts
+++ frontend/server/lib/mapbox/geocode.ts
@@ -1,33 +1,40 @@
-export const lookup = async (address: string) => {
-  const config = useRuntimeConfig();
-  const encodedToken = encodeURIComponent(config.mapboxKey);
-  const encodedAddress = encodeURIComponent(address);
-  const res = await $fetch<{
-    features: {
-      type: "Feature";
-      id: string;
-      geometry: {
-        type: "Point";
-        coordinates: [number, number];
-      };
-      properties: {
-        mapbox_id: string;
-        feature_type: "street";
-        full_address: string;
-        name: string;
-        name_preferred: string;
-        coordinates: { longitude: number; latitude: number };
-        place_formatted: string;
-        context: object;
-      };
-    }[];
-  }>(
-    `https://api.mapbox.com/search/geocode/v6/forward?access_token=${encodedToken}&q=${encodedAddress}&proximity=ip&country=NL`
-  );
+export const lookup = defineCachedFunction(
+  async (address: string) => {
+    const config = useRuntimeConfig();
+    const encodedToken = encodeURIComponent(config.mapboxKey);
+    const encodedAddress = encodeURIComponent(address);
+    const res = await $fetch<{
+      features: {
+        type: "Feature";
+        id: string;
+        geometry: {
+          type: "Point";
+          coordinates: [number, number];
+        };
+        properties: {
+          mapbox_id: string;
+          feature_type: "street";
+          full_address: string;
+          name: string;
+          name_preferred: string;
+          coordinates: { longitude: number; latitude: number };
+          place_formatted: string;
+          context: object;
+        };
+      }[];
+    }>(
+      `https://api.mapbox.com/search/geocode/v6/forward?access_token=${encodedToken}&q=${encodedAddress}&proximity=ip&country=NL`
+    );
 
-  return res.features.map((feature) => ({
-    name: feature.properties.name_preferred,
-    fullAddress: feature.properties.full_address,
-    coordinates: feature.geometry.coordinates,
-  }));
-};
+    return res.features.map((feature) => ({
+      name: feature.properties.name_preferred,
+      fullAddress: feature.properties.full_address,
+      coordinates: feature.geometry.coordinates,
+    }));
+  },
+  {
+    maxAge: 4 * 60 * 60,
+    name: "geocode_lookup",
+    getKey: (address: string) => address,
+  }
+);
